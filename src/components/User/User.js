93698102477a634846e49a02f6141770d0897afe
rylu <rylu@mintel.com>
2017-10-10 09:51:52 +0000

    Change home to user

---
+++ src/components/User/User.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Hero from '../Hero/';
+import { Text, Switch } from 'mintel-react-ui';
+import DocTitle from 'react-document-title';
+
+export default class User extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            alert_on: true
+        };
+
+        this.onSwitch = this.onSwitch.bind(this);
+    }
+
+    onSwitch() {
+        alert.enabled = !this.state.alert_on;
+        this.setState({ alert_on: !this.state.alert_on });
+    }
+
+	render() {
+		return (
+			<div className='User'>
+				<DocTitle title='User Page'/>
+                <Text>hi</Text>
+            </div>
+		);
+	}
+}
---
+++ src/components/User/index.js
@@ -0,0 +1,3 @@
+import './User.scss';
+import Component from './User.js';
+export default Component;
